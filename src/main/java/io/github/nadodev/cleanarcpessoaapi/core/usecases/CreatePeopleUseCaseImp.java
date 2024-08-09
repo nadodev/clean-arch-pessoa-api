342588a0301b2488ec0935854d309dc8208a8134
leoanrdo <programacao@studioalpha.com.br>
2024-08-09 10:23:40 -0300

    chore: add class CreatePeopleUseCaseImp

---
+++ src/main/java/io/github/nadodev/cleanarcpessoaapi/core/usecases/CreatePeopleUseCaseImp.java
@@ -0,0 +1,11 @@
+package io.github.nadodev.cleanarcpessoaapi.core.usecases;
+
+import io.github.nadodev.cleanarcpessoaapi.core.entities.People;
+
+public class CreatePeopleUseCaseImp implements CreatePeopleUseCase {
+
+    @Override
+    public People execute(People people) {
+        return null;
+    }
+}

aa70d895ddf8b16f82f0de04e84d95aae3eb8ba9
leoanrdo <programacao@studioalpha.com.br>
2024-08-09 10:06:17 -0300

    first commit

---
+++ src/main/java/io/github/nadodev/cleanarcpessoaapi/CleanArcPessoaApiApplication.java
@@ -0,0 +1,13 @@
+package io.github.nadodev.cleanarcpessoaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleanArcPessoaApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CleanArcPessoaApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/nadodev/cleanarcpessoaapi/CleanArcPessoaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.nadodev.cleanarcpessoaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleanArcPessoaApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

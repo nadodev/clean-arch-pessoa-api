095a4fc9e220786998508ab941cda91c85b91014
leoanrdo <programacao@studioalpha.com.br>
2024-08-09 10:28:02 -0300

    chore: add injection PeopleGetway in CreatePeopleUseCaseImp

--- src/main/java/io/github/nadodev/cleanarcpessoaapi/core/usecases/CreatePeopleUseCaseImp.java
+++ src/main/java/io/github/nadodev/cleanarcpessoaapi/core/usecases/CreatePeopleUseCaseImp.java
@@ -1,11 +1,19 @@
 package io.github.nadodev.cleanarcpessoaapi.core.usecases;
 
 import io.github.nadodev.cleanarcpessoaapi.core.entities.People;
+import io.github.nadodev.cleanarcpessoaapi.core.getways.PeopleGetway;
 
 public class CreatePeopleUseCaseImp implements CreatePeopleUseCase {
 
+
+    private final PeopleGetway peopleGetway;
+
+    public CreatePeopleUseCaseImp(PeopleGetway peopleGetway) {
+        this.peopleGetway = peopleGetway;
+    }
+
     @Override
     public People execute(People people) {
-        return null;
+        return peopleGetway.create(people);
     }
 }

9263f819e74525e68285daf9e582b8770eb13dc6
leoanrdo <programacao@studioalpha.com.br>
2024-08-09 12:54:43 -0300

    chore: add CreatePeopleUseCase

--- src/main/java/io/github/nadodev/cleanarcpessoaapi/core/usecases/CreatePeopleUseCase.java
+++ src/main/java/io/github/nadodev/cleanarcpessoaapi/core/usecases/CreatePeopleUseCase.java
@@ -1,7 +1,10 @@
 package io.github.nadodev.cleanarcpessoaapi.core.usecases;
 
 import io.github.nadodev.cleanarcpessoaapi.core.entities.People;
+import org.springframework.stereotype.Service;
 
+
+@Service
 public interface CreatePeopleUseCase {
    public People execute(People people);
 }
